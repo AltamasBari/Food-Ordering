59ae6208aff25623cacc033e15d27c915867a6e5
AltamasBari <altamasbari@gmail.com>
2021-12-05 17:41:27 +0530

    test8

--- js/main.js
+++ js/main.js
@@ -1,73 +1,121 @@
-var cart = [];
+const useState = (defaultValue) => {
+  let value = defaultValue;
+  const getValue = () => value;
+  const setValue = (newValue) => (value = newValue);
+  return [getValue, setValue];
+};
 
+const [cart, updCart] = useState(JSON.parse(localStorage.getItem("itemlist")));
+window.onload = function cartsize() {
+  document.getElementById("itemcount").innerText = "(" + cart().length + ")";
+  displayitem();
+};
 function additem(obj) {
   var newitem = {
-    uniqueid: obj.getAttribute("data-uid"),
+    id: obj.getAttribute("data-uid"),
     itemname: obj.getAttribute("data-name"),
     itemprice: obj.getAttribute("data-price"),
+    quantity: 1,
   };
-  cart.push(newitem);
-  document.getElementById("itemcount").innerText = "(" + cart.length + ")";
-  localStorage.setItem("itemlist", JSON.stringify(cart));
+  cart().push(newitem);
+  document.getElementById("itemcount").innerText = "(" + cart().length + ")";
+  updCart(cart());
+  localStorage.setItem("itemlist", JSON.stringify(cart()));
+  obj.innerText = "Added to cart";
+  obj.disabled = true;
+}
+
+function inc(id) {
+  console.log("hi from inc");
+  updCart(
+    cart().map((item) =>
+      item.id == id ? { ...item, quantity: item.quantity + 1 } : item
+    )
+  );
+
+  console.log(cart());
+  localStorage.setItem("itemlist", JSON.stringify(cart()));
+  displayitem();
+}
+
+function dec(id) {
+  console.log("hi from dec");
+
+  updCart(
+    cart().map((item) =>
+      item.id == id
+        ? {
+            ...item,
+            quantity: item.quantity !== 0 ? item.quantity - 1 : item.quantity,
+          }
+        : item
+    )
+  );
+
+  console.log(cart());
+  localStorage.setItem("itemlist", JSON.stringify(cart()));
+  displayitem();
+}
+
+function remove(id) {
+  updCart(
+    cart().filter((item) => {
+      return item.id != id;
+    })
+  );
+  localStorage.setItem("itemlist", JSON.stringify(cart()));
+  displayitem();
 }
 
 function displayitem() {
+  console.log(cart());
   console.log("hiii");
   var total = 0;
   var list1 = document.getElementById("list1");
-  var orderlist = JSON.parse(localStorage.getItem("itemlist"));
-  if (orderlist) {
-    console.log(orderlist);
-    orderlist.map((order) => {
+  list1.innerHTML = "";
+  var headtr = document.createElement("tr");
+  var content = `<th scope="col">Item Name(s)</th>
+  <th scope="col">Decrease qty.</th>
+  <th scope="col">Quantity</th>
+  <th scope="col">Increase qty.</th>
+  <th scope="col">Remove item</th>
+  <th scope="col">Price</th>`;
+  headtr.innerHTML = content;
+  list1.append(headtr);
+  function display() {
+    cart().map((order) => {
       var newlist = document.createElement("tr");
-      var td1 = document.createElement("td");
-      var td2 = document.createElement("td");
-      td1.innerText = order.itemname;
-      td2.innerText = order.itemprice;
-      total += parseInt(order.itemprice.slice(3, 6));
-      newlist.append(td1, td2);
-      list1.append(newlist);
-    });
-    var final = document.createElement("tr");
-    var td3 = document.createElement("th");
-    var td4 = document.createElement("th");
-    td3.innerText = "Total";
-    td4.innerText = "Rs. " + total + " /-";
-    final.append(td3, td4);
-    list1.append(final);
-    return true;
-  } else {
-    return false;
-  }
-}
+      var content1 = `<td>${order.itemname}</td>
+      <td><button class='btn btn-primary' onclick='dec(${
+        order.id
+      })'>-</button></td>
+      <td>${order.quantity}</td>
+      <td><button class="btn btn-primary" onclick='inc(${
+        order.id
+      })'>+</button></td><td>
+      <button class='btn btn-primary' onClick='remove(${
+        order.id
+      })'>Remove</button></td>
+      <td>Rs. ${
+        parseInt(order.itemprice.slice(3, 6)) * order.quantity
+      } /-</td>`;
 
-function opencart() {
-  window.location = "../cart.html";
-  console.log("hiii");
-  var total = 0;
-  var list1 = document.getElementById("list1");
-  var orderlist = JSON.parse(localStorage.getItem("itemlist"));
-  if (orderlist) {
-    console.log(orderlist);
-    orderlist.map((order) => {
-      var newlist = document.createElement("tr");
-      var td1 = document.createElement("td");
-      var td2 = document.createElement("td");
-      td1.innerText = order.itemname;
-      td2.innerText = order.itemprice;
-      total += parseInt(order.itemprice.slice(3, 6));
-      newlist.append(td1, td2);
+      //var td2 = document.createElement("td");
+      // td2.innerText = order.itemprice;
+      newlist.innerHTML = content1;
+      //newlist.append(td1, td2);
       list1.append(newlist);
+
+      total += parseInt(order.itemprice.slice(3, 6)) * order.quantity;
     });
     var final = document.createElement("tr");
     var td3 = document.createElement("th");
+    td3.colSpan = "5";
     var td4 = document.createElement("th");
     td3.innerText = "Total";
     td4.innerText = "Rs. " + total + " /-";
     final.append(td3, td4);
     list1.append(final);
-    return true;
-  } else {
-    return false;
   }
+  display();
 }
